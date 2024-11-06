f694ca0cd9bef7cd8243c0dba6487843406a6907
m7mdragab <mr341349@gmail.com>
2024-11-06 22:22:58 +0200

    display notes part one

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -4,19 +4,11 @@ import 'package:notes_app/constant.dart';
 import 'package:notes_app/cubits/notes_cubit/notes_state.dart';
 import 'package:notes_app/model/note_model.dart';
 
-class NotesCubit extends Cubit {
+class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitialState());
-
+  List<NoteModel>? notes;
   fetchAllNotes() {
-    try {
-      var notesBox = Hive.box<NoteModel>(kNotesBox);
-      List<NoteModel> notes = notesBox.values.toList();
-
-      emit(NotesSuccessState(notes));
-    } on Exception catch (e) {
-      emit(
-        NotesFailureState(e.toString()),
-      );
-    }
+    var notesBox = Hive.box<NoteModel>(kNotesBox);
+    notes = notesBox.values.toList();
   }
 }
